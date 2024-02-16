1f9725e59951b32af3ec1e9b3ede1bdf653b2d3c
Michael Alfonso <michaelalfonso@Michaels-MacBook-Pro-2.local>
2024-02-15 21:07:16 -0500

    change port23 s32221

--- app/index.php
+++ app/index.php
@@ -1,29 +1,20 @@
 <?php
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : "";
+$correo = isset($_POST['correo']) ? $_POST['correo'] : "";
+$mensaje = isset($_POST['mensaje']) ? $_POST['mensaje'] : "";
 
+$port = "5432";
+$host = $_ENV['HOST_DB'];
+$database = $_ENV['DB'];;
+$username = $_ENV['HOST_DB_USER'];
+$password = $_ENV['HOST_DB_PASS'];
+
+$conn = pg_connect("host=$host port=$port dbname=$database user=$username password=$password");
+
+if (!$conn) {
+    echo "Error al conectar a la base de datos: " . pg_last_error($conn);
+    exit();
+} else {
+    echo "Conectado!";
+}
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario HTML básico</title>
-</head>
-<body>
-<h1>Formulario de contacto</h1>
-<form action="/app/index.php" method="post">
-    <label for="nombre">Nombre:</label>
-    <input type="text" id="nombre" name="nombre" placeholder="Introduzca su nombre">
-    <br>
-    <label for="correo">Correo electrónico:</label>
-    <input type="email" id="correo" name="correo" placeholder="Introduzca su correo electrónico">
-    <br>
-    <label for="mensaje">Mensaje:</label>
-    <textarea id="mensaje" name="mensaje" rows="5" cols="30" placeholder="Introduzca su mensaje"></textarea>
-    <br>
-    <button type="submit">Enviar</button>
-</form>
-</body>
-</html>
