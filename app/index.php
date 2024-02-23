99f4650529508d3f77c30be6077886172f9cf0ad
Michael Alfonso <michaelalfonso@Michaels-MacBook-Pro-2.local>
2024-02-22 21:14:22 -0500

    9990------922

--- app/index.php
+++ app/index.php
@@ -6,12 +6,13 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 require_once __DIR__ . "/db.php";
 require_once __DIR__ . "/logic.php";
 
-$metodo = $_SERVER['REQUEST_METHOD'];
+$method = $_SERVER['REQUEST_METHOD'];
 $headers = array_change_key_case(getallheaders(), CASE_LOWER);
 if (array_key_exists('api-key', $headers)) {
     $apiKey = $headers['api-key'];
     if ($apiKey == "9942f9ab-5568-44ce-b63c-2f77b9b7a1e7") {
-        switch ($metodo) {
+        var_dump($method);
+        switch ($method) {
             case 'GET':
                 records();
                 break;
