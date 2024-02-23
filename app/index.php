9006a57c990b22f911aa394a1b6f9f79b7d00687
Michael Alfonso <michaelalfonso@Michaels-MacBook-Pro-2.local>
2024-02-22 19:22:02 -0500

    add records test 222

--- app/index.php
+++ app/index.php
@@ -1,12 +1,12 @@
 <?php
 
-include "./Db.php";
+require_once "./db.php";
 
 $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : "";
 $correo = isset($_POST['correo']) ? $_POST['correo'] : "";
 $mensaje = isset($_POST['mensaje']) ? $_POST['mensaje'] : "";
 
-$db = new Db();
+$db = new db();
 print($db->query("INSERT INTO usuarios (nombre, correo, mensaje) VALUES ('$nombre', '$correo', '$mensaje')"));
 
 ?>
