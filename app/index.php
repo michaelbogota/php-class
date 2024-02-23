927349b37493e62bee7c93d1f3264035a7006c83
Michael Alfonso <michaelalfonso@Michaels-MacBook-Pro-2.local>
2024-02-22 20:39:21 -0500

    9990--

--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 require_once __DIR__ . "/db.php";
 
--- front/scripts.js
+++ front/scripts.js
@@ -5,6 +5,8 @@ form.addEventListener("submit", (event) => {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "https://php-test-0twc.onrender.com/");
     xhr.setRequestHeader("api-key", "9942f9ab-5568-44ce-b63c-2f77b9b7a1e7");
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.onload = () => {
         if (xhr.status === 200) {
             console.log(xhr.responseText);
